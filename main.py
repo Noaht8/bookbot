cb2f92a74e3f0f491cb969f2cd9e5a38fa7913b5
Noaht8 <noahtsegay8@gmail.com>
2025-08-12 19:28:35 +0300

    Final Works

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import sys
 from stats import get_book_text, word_count, num_of_chars
 
-def main(relative_path):
-    contents = get_book_text(relative_path)
-    d = num_of_chars(contents)
-    num_words = word_count(contents)
-    print(f"{num_words} words found in the document")
-    print(d)
+def main(path):
 
-main("/home/noah/workspace/bookbot/books/frankenstein.txt")
+    contents = get_book_text(path)
+    d = num_of_chars(contents)
+    sorted_scores = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+    num_words = word_count(contents)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for c in sorted_scores:
+        print(f"{c}: {sorted_scores[c]}")
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main(sys.argv[1])
